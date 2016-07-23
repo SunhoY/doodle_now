333783a6381dfd89c6d4ba1b16deccf450f6ec5f
Harry Yang <zillaci@gmail.com>
2016-07-23 19:50:47 +0900

    change calculating date range logic. 1. on weekend start time should be last friday. 2. same logic on monday morning before stand up. 3. on weekday morning before stand up queries range from yesterday after stand up to now. 4. on weekday morning after stand up queries range from today stand up to now.

--- app/src/main/java/io/harry/doodlenow/fragment/doodlerange/DoodleRangeCalculator.java
+++ app/src/main/java/io/harry/doodlenow/fragment/doodlerange/DoodleRangeCalculator.java
@@ -8,25 +8,78 @@ public class DoodleRangeCalculator {
     public DoodleRange calculateRange(DoodleListType type, DateTime current, int standUpStartsAt, int archiveDuration) {
         long startAt;
         long endAt;
-        DateTime standUpStartTime = current.withTimeAtStartOfDay().plusHours(standUpStartsAt);
-        if(type == DoodleListType.Today) {
-            if (current.getHourOfDay() < standUpStartsAt) {
-                startAt = standUpStartTime.minusDays(1).getMillis();
-            } else {
-                startAt = standUpStartTime.getMillis();
-            }
 
+        if(type == DoodleListType.Today) {
+            startAt = calculateStartTimeForToday(current, standUpStartsAt);
             endAt = current.getMillis();
         } else {
-            if (current.getHourOfDay() < standUpStartsAt) {
-                startAt = standUpStartTime.minusDays(archiveDuration + 1).getMillis();
-                endAt = standUpStartTime.minusDays(1).getMillis();
-            } else {
-                startAt = standUpStartTime.minusDays(archiveDuration).getMillis();
-                endAt = standUpStartTime.getMillis();
-            }
+            startAt = calculateStartTimeForArchive(current, standUpStartsAt, archiveDuration);
+            endAt = calculateEndTimeForArchive(current, standUpStartsAt);
         }
 
         return new DoodleRange(startAt, endAt);
+    }
+
+    private long calculateStartTimeForToday(DateTime current, int standUpStartsAt) {
+        long startTimeMillis;
+        boolean isWeekendOrFirstWorkingDay = getWeekendOrFirstWorkingDay(current, standUpStartsAt);
+
+        DateTime standUpStartTime = current.withTimeAtStartOfDay().plusHours(standUpStartsAt);
+
+        if(isWeekendOrFirstWorkingDay) {
+            DateTime lastWorkingDay = getLastFriday(current);
+
+            startTimeMillis = standUpStartTime.withDayOfMonth(lastWorkingDay.getDayOfMonth()).getMillis();
+        } else if (current.getHourOfDay() < standUpStartsAt) {
+            startTimeMillis = standUpStartTime.minusDays(1).getMillis();
+        } else {
+            startTimeMillis = standUpStartTime.getMillis();
+        }
+        return startTimeMillis;
+    }
+
+    private boolean getWeekendOrFirstWorkingDay(DateTime dateTime, int standUpStartsAt) {
+        int dayOfWeek = dateTime.getDayOfWeek();
+        if(dayOfWeek == 6 || dayOfWeek == 7) {
+            return true;
+        } else if(dayOfWeek == 1) {
+            return dateTime.getHourOfDay() < standUpStartsAt;
+        }
+
+        return false;
+    }
+
+    private DateTime getLastFriday(DateTime dateTime) {
+        DateTime result = new DateTime(dateTime.getMillis());
+
+        while(result.getDayOfWeek() != 5) {
+            result = result.minusDays(1);
+        }
+        return result;
+    }
+
+    private long calculateStartTimeForArchive(DateTime current, int standUpStartsAt, int archiveDuration) {
+        long startAt;
+        DateTime standUpStartTime = current.withTimeAtStartOfDay().plusHours(standUpStartsAt);
+
+        if (current.getHourOfDay() < standUpStartsAt) {
+            startAt = standUpStartTime.minusDays(archiveDuration + 1).getMillis();
+        } else {
+            startAt = standUpStartTime.minusDays(archiveDuration).getMillis();
+        }
+
+        return startAt;
+    }
+
+    private  long calculateEndTimeForArchive(DateTime current, int standUpStartsAt) {
+        DateTime standUpStartTime = current.withTimeAtStartOfDay().plusHours(standUpStartsAt);
+        long endAt;
+        if (current.getHourOfDay() < standUpStartsAt) {
+            endAt = standUpStartTime.minusDays(1).getMillis();
+        } else {
+            endAt = standUpStartTime.getMillis();
+        }
+
+        return endAt;
     }
 }
--- app/src/test/java/io/harry/doodlenow/fragment/doodlerange/DoodleRangeCalculatorTest.java
+++ app/src/test/java/io/harry/doodlenow/fragment/doodlerange/DoodleRangeCalculatorTest.java
@@ -1,56 +1,80 @@
 package io.harry.doodlenow.fragment.doodlerange;
 
 import org.joda.time.DateTime;
+import org.junit.Before;
 import org.junit.Test;
 
 import io.harry.doodlenow.fragment.DoodleListType;
 
+import static io.harry.doodlenow.fragment.DoodleListType.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DoodleRangeCalculatorTest {
+    private static final long MILLIS_2016_06_21_10_00_00 = 1466470800000L;
+    private static final long MILLIS_2016_06_21_09_59_59 = 1466470799000L;
+    private static final long MILLIS_2016_06_20_10_00_00 = 1466384400000L;
+    private static final long MILLIS_2016_06_20_09_59_59 = 1466384399000L;
     private static final long MILLIS_2016_06_19_10_00_00 = 1466298000000L;
     private static final long MILLIS_2016_06_19_09_59_59 = 1466297999000L;
     private static final long MILLIS_2016_06_18_10_00_00 = 1466211600000L;
+    private static final long MILLIS_2016_06_17_10_00_00 = 1466125200000L;
     private static final long MILLIS_2016_06_12_10_00_00 = 1465693200000L;
     private static final long MILLIS_2016_06_11_10_00_00 = 1465606800000L;
     private static final int STAND_UP_STARTS_AT_10AM = 10;
     private static final int ARCHIVES_FOR_7_DAYS = 7;
     private DoodleRangeCalculator subject;
 
+    @Before
+    public void setUp() throws Exception {
+        subject = new DoodleRangeCalculator();
+    }
+
     @Test
     public void calculateRange_returnsYesterdayStandUpTimeToNow_whenTypeIsTodayAndBeforeStandUp() throws Exception {
-        subject = new DoodleRangeCalculator();
-
         DoodleRange result = subject.calculateRange(
-                DoodleListType.Today,
-                new DateTime(MILLIS_2016_06_19_09_59_59),
+                Today,
+                new DateTime(MILLIS_2016_06_21_09_59_59),
                 STAND_UP_STARTS_AT_10AM,
                 ARCHIVES_FOR_7_DAYS);
 
-        assertThat(result.getStartAt()).isEqualTo(MILLIS_2016_06_18_10_00_00);
-        assertThat(result.getEndAt()).isEqualTo(MILLIS_2016_06_19_09_59_59);
+        assertThat(result.getStartAt()).isEqualTo(MILLIS_2016_06_20_10_00_00);
+        assertThat(result.getEndAt()).isEqualTo(MILLIS_2016_06_21_09_59_59);
     }
 
     @Test
     public void calculateRange_returnsTodayStandUpTimeToNow_whenTypeIsTodayAndAfterStandUp() throws Exception {
-        subject = new DoodleRangeCalculator();
-
         DoodleRange result = subject.calculateRange(
-                DoodleListType.Today,
-                new DateTime(MILLIS_2016_06_19_10_00_00),
+                Today,
+                new DateTime(MILLIS_2016_06_21_10_00_00),
                 STAND_UP_STARTS_AT_10AM,
                 ARCHIVES_FOR_7_DAYS);
 
-        assertThat(result.getStartAt()).isEqualTo(MILLIS_2016_06_19_10_00_00);
+        assertThat(result.getStartAt()).isEqualTo(MILLIS_2016_06_21_10_00_00);
+        assertThat(result.getEndAt()).isEqualTo(MILLIS_2016_06_21_10_00_00);
+    }
+
+    @Test
+    public void calculateRange_returnsLastWorkingDateAsStartAt_whenTypeIsTodayAndIsMondayAndBeforeStandUp() throws Exception {
+        DoodleRange result = subject.calculateRange(
+                Today, new DateTime(MILLIS_2016_06_19_10_00_00), STAND_UP_STARTS_AT_10AM, ARCHIVES_FOR_7_DAYS);
+
+        assertThat(result.getStartAt()).isEqualTo(MILLIS_2016_06_17_10_00_00);
         assertThat(result.getEndAt()).isEqualTo(MILLIS_2016_06_19_10_00_00);
     }
 
     @Test
-    public void calculateRange_returnsArchiveDurationDaysBeforeYesterday_whenTypeIsArchiveAndBeforeStandUp() throws Exception {
-        subject = new DoodleRangeCalculator();
-
+    public void calculateRange_returnsLastWorkingDateAsStartAt_whenTypeIsTodayAndIsWeekend() throws Exception {
         DoodleRange result = subject.calculateRange(
-                DoodleListType.Archive,
+                Today, new DateTime(MILLIS_2016_06_20_09_59_59), STAND_UP_STARTS_AT_10AM, ARCHIVES_FOR_7_DAYS);
+
+        assertThat(result.getStartAt()).isEqualTo(MILLIS_2016_06_17_10_00_00);
+        assertThat(result.getEndAt()).isEqualTo(MILLIS_2016_06_20_09_59_59);
+    }
+
+    @Test
+    public void calculateRange_returnsArchiveDurationDaysBeforeYesterday_whenTypeIsArchiveAndBeforeStandUp() throws Exception {
+        DoodleRange result = subject.calculateRange(
+                Archive,
                 new DateTime(MILLIS_2016_06_19_09_59_59),
                 STAND_UP_STARTS_AT_10AM,
                 ARCHIVES_FOR_7_DAYS);
@@ -61,10 +85,8 @@ public class DoodleRangeCalculatorTest {
 
     @Test
     public void calculateRange_returnsArchiveDurationBeforeToday_whenTypeIsArchiveAndAfterStandUp() throws Exception {
-        subject = new DoodleRangeCalculator();
-
         DoodleRange result = subject.calculateRange(
-                DoodleListType.Archive,
+                Archive,
                 new DateTime(MILLIS_2016_06_19_10_00_00),
                 STAND_UP_STARTS_AT_10AM,
                 ARCHIVES_FOR_7_DAYS);
